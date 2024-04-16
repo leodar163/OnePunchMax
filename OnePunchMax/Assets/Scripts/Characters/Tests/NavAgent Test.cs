531d0e34bdd70e8dd27699ede1e2d8cbe882401f
leodar163 <leobgely@gmail.com>
2024-04-16 11:33:54 +0200

    add path finding

---
+++ OnePunchMax/Assets/Scripts/Characters/Tests/NavAgent Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Characters.Tests
+{
+    public class NavAgentTest : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent _navMeshAgent;
+        [SerializeField] private Transform _target;
+        
+        private void Update()
+        {
+            _navMeshAgent.destination = _target.position;
+        }
+    }
+}
