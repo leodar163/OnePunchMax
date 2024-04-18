7e4f76ee8ff44838476a9e6d923ea6279ce36ee2
leodar163 <leobgely@gmail.com>
2024-04-18 16:39:25 +0200

    add animation system for characters

--- OnePunchMax/Assets/Scripts/Behaviors/Animations/CharacterAnimator.cs
+++ OnePunchMax/Assets/Scripts/Behaviors/Animations/CharacterAnimator.cs
@@ -12,6 +12,8 @@ namespace Behaviors.Animations
         private static readonly int DirectionAnimArg = Animator.StringToHash("Direction");
         private static readonly int SpeedAnimArg = Animator.StringToHash("Speed");
 
+        [SerializeField] private bool _lookByAim;
+        
         private void Update()
         {
             ManagerLookAt();
@@ -19,7 +21,17 @@ namespace Behaviors.Animations
 
         private void ManagerLookAt()
         {
-            float directionAngle = Mathf.Atan2(_controller.AimingDirection.y, _controller.AimingDirection.x) * Mathf.Rad2Deg;
+            float directionAngle = 0;
+            if (_lookByAim)
+            {
+                directionAngle = Mathf.Atan2(_controller.AimingDirection.y, _controller.AimingDirection.x) * Mathf.Rad2Deg;
+            }
+            else
+            {
+                Vector2 movementDirection = _controller.GetMovement();
+                directionAngle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg;
+            }
+            
             float direction = Mathf.DeltaAngle(0, directionAngle) switch
             {
                 < 45 and >= -45 => 1,
@@ -30,8 +42,13 @@ namespace Behaviors.Animations
                 _ => throw new ArgumentOutOfRangeException()
             };
             
-            //_animator.SetFloat(DirectionAnimArg, direction);
+            _animator.SetFloat(DirectionAnimArg, direction);
             _animator.SetFloat(SpeedAnimArg, _controller.GetMovement().magnitude > 0.01f ? 1 : 0);
+        }
+
+        public void SwitchLookMode()
+        {
+            _lookByAim = !_lookByAim;
         }
     }
 }
